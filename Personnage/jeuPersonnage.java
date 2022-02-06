ef54b6841b10c419953002ce170a6647931677a6
matthiashautin <98103760+matthiashautin@users.noreply.github.com>
2022-02-06 21:33:36 +0100

    Update jeuPersonnage.java

--- Personnage/jeuPersonnage.java
+++ Personnage/jeuPersonnage.java
@@ -13,27 +13,24 @@ public class jeuPersonnage {
 			System.out.println(Bot.LeNom() + " ------> LIFE = " + Bot.LaVie());
 			System.out.println(Bot.LeNom() + " ------> LEVEL = " + Bot.Level());
 			System.out.println(Bot.LeNom() + " ------> XP = " + Bot.xp());
-			sc.nextLine();
+			sc.nextLine();//Appuyer sur [Entrer] pour continuer
 			System.out.println(NewPersonnage.LeNom() + " se balade dans la forêt et croise un " + Bot.LeNom() + " colérique");
-			sc.nextLine();
+			sc.nextLine();//Appuyer sur [Entrer] pour continuer
 			System.out.println(NewPersonnage.LeNom() + " attaque " + Bot.LeNom() + " et lui infllige " + NewPersonnage.nb_degat() +" degats");
 			Bot.Degat(100);//le bot perd 100 de life
-			sc.nextLine();
+			sc.nextLine();//Appuyer sur [Entrer] pour continuer
 			System.out.println("Avant de mourir le " + Bot.LeNom() + " inflige " + Bot.nb_degat() + " degats a " + NewPersonnage.LeNom());
 			NewPersonnage.Degat(75);//NewPersonnage perd 75 de life
-			sc.nextLine();
+			sc.nextLine();//Appuyer sur [Entrer] pour continuer
 			System.out.println(NewPersonnage.LeNom() + " a " + NewPersonnage.LaVie()+ " de vie ");
 			System.out.println(Bot.LeNom() + " a " + Bot.LaVie()+ " de vie ");
 
-			//si Bot == 0LIFE alors NewPersonnage ++ 100xp and 50LIFE
-			if ( Bot.LaVie() == 0){
+			if ( Bot.LaVie() == 0){//si Bot == 0LIFE alors NewPersonnage ++ 100xp and 75LIFE
 				System.out.println("Le " + Bot.LeNom() + " vient de mourir");
 				NewPersonnage.xpsup(100);
 				NewPersonnage.Healer(75);
 			}
-
-			//si NewPersonnage == 100xp alors NewPersonnage ++ 1LEVEL
-			if (NewPersonnage.xp() == 100) {
+			if (NewPersonnage.xp() == 100) {//si NewPersonnage == 100xp alors NewPersonnage + 1LEVEL
 				NewPersonnage.Levelsup(1);
 			}
 		
